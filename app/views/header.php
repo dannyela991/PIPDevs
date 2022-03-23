a47b6a6a8cda83d625cd1488bb31b4b1cb60b3d6
Daniela <dannyela991@gmail.com>
2022-03-23 15:22:18 -0300

    Atualização

--- app/views/header.php
+++ app/views/header.php
@@ -27,7 +27,7 @@
         <div class="container">
             <div class="header-nav">
                 <div class="logotipo">
-                    <a href="home.php" title="HomePage">
+                    <a href="index.html" title="HomePage">
                         <img src="app/assets/images/logotipo.svg" alt="Logotipo"/>
                     </a>
                 </div>
