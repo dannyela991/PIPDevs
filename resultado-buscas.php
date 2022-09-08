b254ab3d1e97c0f9d38bff9be90b72bd9971f715
Daniela <dannyela991@gmail.com>
2022-09-08 17:47:31 -0300

    Criado pg de resultado geral ajustes no CSS

---
+++ resultado-buscas.php
@@ -0,0 +1,213 @@
+<?php
+// header
+include 'app/views/header.php';
+?>
+<!-- content -->
+
+<main>
+    <section class="homenagens list-projects fases archives page-resultados">
+        <div class="page-header resultado">
+            <div class="container">
+                <h1><small>Resutado de busca</small></h1> 
+            </div>
+        </div>
+        <div  class="container">
+            <div class="txt-resultado">
+                <p>Você pesquisou por <span>Notícia</span>&nbsp;&nbsp;&nbsp;<a href=""> <i class="far fa-times-circle"></i> Limpar</a></p>
+                <!-- not result -->
+                <p>Não encontramos nenhum resultado referente a sua busca</p>
+                <!-- not result -->
+            </div>
+            <div class="content result">
+                <div id="result-noticias">
+                    <h3>Resultado encontrado em Notícias</h3>
+                    <div class="content-archives">
+                        <div class="card posts">
+                            <a href="media-single.php">
+                                <div class="img-card">
+                                    <span class="category">Vídeo</span>
+                                </div>                                
+                            </a>                            
+                            <div class="description-article">
+                                <a href="media-single.php"><h2>Noruega e o luxo inusitado IV com Liliana Calisto e Maria Alarcon de V.O.S.</h2></a>
+                                <p>Antonella Braillard convida a equipe de V.O.S (DMC) para falar sobre os serviços que eles oferecem para os passageiros mais exigentes. Venha descobrir uma variedade de atividades que pode-se incluir ao considerar a Escandinávia como destino.</p>
+                                <div class="read-more">
+                                    <a class="link-archives" href="media-single.php">Assistir</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card posts">
+                            <a href="media-single.php">
+                                <div class="img-card">
+                                    <span class="category">Vídeo</span>
+                                </div>                                
+                            </a>                            
+                            <div class="description-article">
+                                <a href="media-single.php"><h2>Noruega e o luxo inusitado IV com Liliana Calisto e Maria Alarcon de V.O.S.</h2></a>
+                                <p>Antonella Braillard convida a equipe de V.O.S (DMC) para falar sobre os serviços que eles oferecem para os passageiros mais exigentes. Venha descobrir uma variedade de atividades que pode-se incluir ao considerar a Escandinávia como destino.</p>
+                                <div class="read-more">
+                                    <a class="link-archives" href="media-single.php">Assistir</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card posts">
+                            <a href="media-single.php">
+                                <div class="img-card">
+                                    <span class="category">Vídeo</span>
+                                </div>                                
+                            </a>                            
+                            <div class="description-article">
+                                <a href="media-single.php"><h2>Noruega e o luxo inusitado IV com Liliana Calisto e Maria Alarcon de V.O.S.</h2></a>
+                                <p>Antonella Braillard convida a equipe de V.O.S (DMC) para falar sobre os serviços que eles oferecem para os passageiros mais exigentes. Venha descobrir uma variedade de atividades que pode-se incluir ao considerar a Escandinávia como destino.</p>
+                                <div class="read-more">
+                                    <a class="link-archives" href="media-single.php">Assistir</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card posts">
+                            <a href="media-single.php">
+                                <div class="img-card">
+                                    <span class="category">Vídeo</span>
+                                </div>                                
+                            </a>                            
+                            <div class="description-article">
+                                <a href="media-single.php"><h2>Noruega e o luxo inusitado IV com Liliana Calisto e Maria Alarcon de V.O.S.</h2></a>
+                                <p>Antonella Braillard convida a equipe de V.O.S (DMC) para falar sobre os serviços que eles oferecem para os passageiros mais exigentes. Venha descobrir uma variedade de atividades que pode-se incluir ao considerar a Escandinávia como destino.</p>
+                                <div class="read-more">
+                                    <a class="link-archives" href="media-single.php">Assistir</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <div id="result-projetos">
+                    <h3>Resultado encontrado em Projetos</h3>
+                    <div class="content-archives">
+                        <div class="card-project">
+                            <div class="title-project flex-container">
+                                <div class="logo-project">
+                                    <a href="project-single.php"><img src="app/assets/images/gva.png" alt=""/></a>
+                                </div>
+                                <div class="title">                                        
+                                    <a href="project-single.php"><h4>Ecoponto de entrega voluntária</h4></a>
+                                    <p class="eixo">Cidades</p>
+                                </div>
+                            </div>
+                            <div class="description-project">
+                                <p class="city-uf">TRÊS COROAS | RS</p>
+                                <p>Promover a coleta seletiva nas escolas e entidades com foco na educação ambiental, através do incentivo da reciclagem, economia solidária e sustentabilidade.</p>
+                                <div class="img-ods">
+                                    <ul>
+                                        <li><img src="app/assets/images/img-ods/01.png" alt=""/></li>
+                                        <li><img src="app/assets/images/img-ods/04.png" alt=""/></li>
+                                        <li><img src="app/assets/images/img-ods/09.png" alt=""/></li>
+                                        <li><img src="app/assets/images/img-ods/16.png" alt=""/></li>
+                                    </ul>
+                                </div>
+                                <div class="list-tags">
+                                    <ul>
+                                        <li>categoria</li>
+                                        <li>categoria</li>
+                                        <li>categoria</li>
+                                        <li>categoria</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                            <div class="votos"><span class="number-votos">3521</span> voto(s)</div>
+                        </div>
+                    </div>
+                </div>
+                <div id="result-homenagens">
+                    <h3>Resultado encontrado em Homenagens</h3>
+                    <!-- CIDADÃO DE IMPACTO -->
+                    <div class="list-item">
+                        <div class="flex-container">
+                            <div class="votos">
+                                <span class="number-votos">521</span>
+                                <br>indicações
+                            </div>
+                            <div class="dados">
+                                <p class="name">Nome Cidadão de Impacto</p>
+                                <p class="categoria-honor">Cidadão de Impacto</p>
+                                <p class="indicacao">Indicado por: <span>Liderança Ambiental</span></p> 
+                                <ul class="list-social">
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="btn-voto">
+                                <button type="button" class="btn"><i class="far fa-thumbs-up"></i> Indicar</button>   
+                                <a href="#" class="gray">Quero indicar para outra categoria</a>                         
+                            </div>
+                        </div>                                                      
+                    </div>         
+                    <!-- CIDADÃO DE IMPACTO -->
+
+                    <!-- INSTITUIÇÃO DE IMPACTO -->
+                    <div class="list-item">
+                        <div class="flex-container">
+                            
+                            <div class="votos">
+                                <span class="number-votos">521</span>
+                                <br>indicações
+                            </div>
+                            <div class="dados">
+                                <p class="name">Nome Instituição de Impacto</p>
+                                <p class="categoria-honor">Instituição de Impacto</p>
+                                <p class="indicacao">Indicado por: <span>Liderança Ambiental</span></p> 
+                                <ul class="list-social">
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="btn-voto">
+                                <button type="button" class="btn"><i class="far fa-thumbs-up"></i> Indicar</button>   
+                                <a href="#" class="gray">Quero indicar para outra categoria</a>                         
+                            </div>
+                        </div>                                                       
+                    </div>
+                    <!-- INSTITUIÇÃO DE IMPACTO -->
+
+                    <!-- COMUNICADOR DE IMPACTO -->
+                    <div class="list-item">
+                        <div class="flex-container">
+                            <div class="votos">
+                                <span class="number-votos">521</span>
+                                <br>indicações
+                            </div>
+                            <div class="dados">
+                                <p class="name">Nome Comunicador de Impacto</p>
+                                <p class="categoria-honor">Comunicador de Impacto</p>
+                                <p class="indicacao">Indicado por: <span>Liderança Ambiental</span></p> 
+                                <ul class="list-social">
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="btn-voto">
+                                <button type="button" class="btn"><i class="far fa-thumbs-up"></i> Indicar</button>
+                                <a href="#" class="gray">Quero indicar para outra categoria</a>                         
+                            </div>
+                        </div>                                                      
+                    </div>
+                    <!-- COMUNICADOR DE IMPACTO -->
+                </div>
+                
+            </div>
+            
+            </div>
+        </section>
+    </main>
+        
+        
+    <?php
+    // footer
+    include 'app/views/footer.php';
+    ?>
