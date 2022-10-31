ebac945b917b908bf29ce03594dace80bcc4fa72
Daniela <dannyela991@gmail.com>
2022-10-31 11:04:18 -0300

    criado página de eventos atualização CSS

---
+++ eventos.php
@@ -0,0 +1,38 @@
+<?php
+// header
+include 'app/views/header.php';
+?>
+<!--  content -->
+<main>
+    <section class="pages workshop eventos">
+        <div class="page-header">
+            <div class="container">
+                <h1>Impactos Positivos Day</h1>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="text-evento">
+                <p>Chegamos ao último passo de nossa premiação, iremos realizar a cerimônia de encerramento e a entrega dos prêmios aos vencedores de cada categoria / cada sub-categoria.</p>
+            </div>
+            <div class="box-cortina"></div>
+            <div class="flex-container box-video">
+                <div class="video">
+                    <div class="container-iframe"> 
+                        <iframe class="responsive-iframe" src="https://vimeo.com/event/2472241/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+                    </div>
+                </div>
+                <div class="chat">
+                    <iframe src="https://vimeo.com/event/2472241/chat/" width="100%" height="450" frameborder="0"></iframe>
+                </div>
+            </div>
+            <!-- <div class="box-palco"></div> -->
+        </div>
+    </section>
+    
+</main>
+
+<?php
+// footer
+include 'app/views/footer.php';
+?>
---
+++ save-date.php
@@ -0,0 +1,184 @@
+<?php
+// header
+include 'app/views/header.php';
+?>
+<!--  content -->
+<main>
+    <section class="pages workshop eventos">
+        <div class="page-header">
+            <div class="container">
+                <h1>EM BREVE<br> Impactos Positivos Day</h1>
+                <p> Save The Date: <strong>30 de novembro!</strong></p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="text-evento">
+                <p>No dia <strong>30 de novembro de 2022</strong>, às <strong>08:30(am)</strong>, começa a transmissão do evento final do <strong>Prêmio Impactos Positivos 2022</strong>, onde serão divulgados os vencedores de cada categoria desta edição.</p>
+                <p>Durante o evento, também teremos palestras e conversas com incríveis e importantes profissionais que atuam em nosso <strong>Ecossistema de Impacto</strong>, para dividir experiências, tendências e informações necessárias para o fomento do setor.</p>
+                <h2>Em breve mais novidades por aqui!</h2>
+            </div>
+            <!-- <div class="flex-container">
+                <div class="video">
+                    <div class="container-iframe"> 
+                        <iframe class="responsive-iframe" src="https://vimeo.com/event/2472241/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+                    </div>
+                </div>
+                <div class="chat">
+                    <iframe src="https://vimeo.com/event/2472241/chat/" width="100%" height="100%" frameborder="0"></iframe>
+                </div>
+            </div> -->
+        </div>
+    </section>
+
+    <section class="patrocinador eventos">
+        <div class="container flex-container">
+            <div class="col-sponsors">
+                <div class="title">
+                    <h3>Patrocinador</h3>
+                </div>    
+                <div class="logo-sponsors">
+                    <div class="img-sponsors">
+                        <img src="app/assets/images/sebrae.png" alt=""/>
+                    </div>
+                    <div class="img-sponsors">
+                        <img src="app/assets/images/cubo-esg.jpg" alt=""/>
+                    </div>
+                </div>
+            </div> 
+            <div class="col-sponsors">
+                <div class="title">
+                    <h3>Parceiros Institucionais</h3>
+                </div>    
+                <div class="col-articles flex-container">
+                    <div class="img-parceiro">
+                        <img src="app/assets/images/enimpactos.png" alt="Enimpacto"/>
+                    </div>
+                    <div class="img-parceiro">
+                        <img src="app/assets/images/logo-pnud.png" alt="pnud"/>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </section>
+
+    <section class="further apoiadores">
+        <div class="container">
+            <div class="col-further-content">
+                <div class="title">
+                    <h2>Apoiadores</h2>
+                </div>
+            </div>
+            <div class="link-logos">
+                <div class="carrossel-further">
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/innov8.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/assis-mendes.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/humanizadas.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/instituto da transformaco digital.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/capitalismo-consciente.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/comunidade-cuidadoria.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/gonew.png" alt=""/></a>
+                        </div>
+                    </div>
+                </div>                   
+                <div class="nav-further">
+                    <a id="prev-further" class="icon arrow-left">
+                        <i class="fas fa-chevron-left"></i>
+                    </a>
+                    <a id="next-further" class="icon arrow-right">
+                        <i class="fas fa-chevron-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="further">
+        <div class="container">
+            <div class="col-further-content">
+                <div class="title">
+                    <h2>Corrente do bem</h2>
+                </div>
+            </div>
+            <div class="link-logos">
+                <div class="carrossel-logos">
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/innov8.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/assis-mendes.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/humanizadas.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/instituto da transformaco digital.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/capitalismo-consciente.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/comunidade-cuidadoria.png" alt=""/></a>
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="img-logo">
+                            <a href=""><img src="app/assets/images/logo-further/gonew.png" alt=""/></a>
+                        </div>
+                    </div>
+                </div>                   
+                <div class="nav-further">
+                    <a id="prev-link-logos" class="icon arrow-left">
+                        <i class="fas fa-chevron-left"></i>
+                    </a>
+                    <a id="next-link-logos" class="icon arrow-right">
+                        <i class="fas fa-chevron-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+</main>
+
+<?php
+// footer
+include 'app/views/footer.php';
+?>
